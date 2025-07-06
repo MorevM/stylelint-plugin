801996bb93eaa1cc05448a06f63cec8a8ddf52be
MorevM <max.seainside@gmail.com>
2025-07-07 02:06:18 +0300

    wip(no-block-properties): Base logic and tests

--- src/rules/no-block-properties/no-block-properties.test.ts
+++ src/rules/no-block-properties/no-block-properties.test.ts
@@ -8,57 +8,104 @@ testRule({
 	config: [true],
 	accept: [
 		{
-			description: 'No external geometry in the component',
+			description: 'No disallowed properties in the component block or its modifiers',
 			code: `
-				.the-component {
-					color: red;
-				}
+				.the-component { color: red; }
+				.the-component--modifier { color: red; }
+				.the-component--modifier--value { color: red; }
 			`,
 		},
 		{
-			description: 'External geometry property in Element or its modifier',
+			description: 'Disallowed property in element or its modifier',
 			code: `
-				.the-component {
-					color: red;
-
-					&__element {
-						margin-block-start: 16px;
-
-						&--element-modifier {
-							margin-block-start: 8px;
-						}
-					}
-				}
+				.the-component__element { margin-block-start: 16px; }
+				.the-component__element--modifier { margin-block-start: 16px; }
 			`,
 		},
 		{
-			description: 'External geometry property in a nested rule',
+			description: 'Disallowed property in a side-effect',
+			code: `
+				.the-component a { margin-block-start: 16px; }
+				.the-component .another-component { margin-block-start: 16px; }
+			`,
+		},
+		{
+			description: 'Side effect applied to its own modifier or element',
+			code: `
+				.the-component .the-component__element { margin-block-start: 16px; }
+				.the-component .the-component--modifier { margin-block-start: 16px; }
+			`,
+		},
+		{
+			description: 'Disallowed property in a side-effect using nesting',
 			code: `
 				.the-component {
+					a { margin-block-start: 16px; }
 					.another-component { margin-block-start: 16px; }
-					img { margin-block-start: 16px; }
 				}
+			`,
+		},
+		{
+			description: 'Disallowed property in pseudo-element',
+			code: `
+				.the-component::before { margin-block-start: 16px; }
+				.the-component--modifier::before { margin-block-start: 16px; }
 			`,
 		},
 	],
 	reject: [
 		{
-			description: 'Block has an external geometry property',
+			description: 'Block has a disallowed property',
 			code: `
-				.the-component {
-					margin-block-start: 16px;
-				}
+				.the-component { margin-block-start: 16px; }
 			`,
 			warnings: [
 				{
-					message: messages.unexpected('margin-block-start'),
-					line: 2, column: 2,
-					endLine: 2, endColumn: 20,
+					message: messages.unexpected('margin-block-start', '.the-component', 'EXTERNAL_GEOMETRY'),
+					line: 1, column: 18,
+					endLine: 1, endColumn: 36,
 				},
 			],
 		},
 		{
-			description: 'Block modifier has an external geometry property',
+			description: 'Multiple blocks have a disallowed property using compound selector',
+			code: `
+				.the-component, .foo { margin-block-start: 16px; }
+			`,
+			warnings: [
+				{
+					message: messages.unexpected('margin-block-start', '.the-component', 'EXTERNAL_GEOMETRY'),
+					line: 1, column: 24,
+					endLine: 1, endColumn: 42,
+				},
+				{
+					message: messages.unexpected('margin-block-start', '.foo', 'EXTERNAL_GEOMETRY'),
+					line: 1, column: 24,
+					endLine: 1, endColumn: 42,
+				},
+			],
+		},
+		{
+			description: 'One of compound selectors has a disallowed property',
+			code: `
+				.the-component, .foo__element { margin-block-start: 16px; }
+				.the-component__element, .foo { margin-block-start: 16px; }
+			`,
+			warnings: [
+				{
+					message: messages.unexpected('margin-block-start', '.the-component', 'EXTERNAL_GEOMETRY'),
+					line: 1, column: 33,
+					endLine: 1, endColumn: 51,
+				},
+				{
+					message: messages.unexpected('margin-block-start', '.foo', 'EXTERNAL_GEOMETRY'),
+					line: 2, column: 33,
+					endLine: 2, endColumn: 51,
+				},
+			],
+		},
+		{
+			description: 'Block modifier has disallowed property',
 			code: `
 				.the-component {
 					&--modifier {
@@ -68,7 +115,24 @@ testRule({
 			`,
 			warnings: [
 				{
-					message: messages.unexpected('margin-block-start'),
+					message: messages.unexpected('margin-block-start', '.the-component--modifier', 'EXTERNAL_GEOMETRY'),
+					line: 3, column: 3,
+					endLine: 3, endColumn: 21,
+				},
+			],
+		},
+		{
+			description: 'Block modifier nested inside another selector with disallowed property',
+			code: `
+				.the-component {
+					&--modifier.is-active {
+						margin-block-start: 16px;
+					}
+				}
+			`,
+			warnings: [
+				{
+					message: messages.unexpected('margin-block-start', '.the-component--modifier.is-active', 'EXTERNAL_GEOMETRY'),
 					line: 3, column: 3,
 					endLine: 3, endColumn: 21,
 				},
@@ -85,9 +149,39 @@ testRule({
 			`,
 			warnings: [
 				{
-					message: messages.unexpected('margin-block-start'),
+					message: messages.unexpected('margin-block-start', '.the-component', 'EXTERNAL_GEOMETRY'),
 					line: 3, column: 3,
 					endLine: 3, endColumn: 21,
+				},
+			],
+		},
+	],
+});
+
+testRule({
+	description: '`ignoreBlocks` option',
+	config: [true, { ignoreBlocks: ['swiper-*', /.*legacy.*/] }],
+	accept: [
+		{
+			description: 'Does not report ignored blocks',
+			code: `
+				.swiper-slide { margin-block-start: 16px; }
+				.foo-legacy-component { margin-block-start: 16px; }
+				.legacy-component { margin-block-start: 16px; }
+			`,
+		},
+	],
+	reject: [
+		{
+			description: 'Still reports non-ignored ones',
+			code: `
+				.the-component { margin-block-start: 16px; }
+			`,
+			warnings: [
+				{
+					message: messages.unexpected('margin-block-start', '.the-component', 'EXTERNAL_GEOMETRY'),
+					line: 1, column: 18,
+					endLine: 1, endColumn: 36,
 				},
 			],
 		},
--- src/rules/no-block-properties/no-block-properties.ts
+++ src/rules/no-block-properties/no-block-properties.ts
@@ -1,44 +1,52 @@
+import { isEmpty, tsObject } from '@morev/utils';
 import resolveNestedSelector from 'postcss-resolve-nested-selector';
 import * as v from 'valibot';
-import { addNamespace, createRule, findRuleDeclarations, getRuleUrl } from '#utils';
+import { addNamespace, createRule, findRuleDeclarations, getRuleUrl, isPseudoElementNode, parseSelectors, resolveBemEntities, toRegExp } from '#utils';
+import { vSeparatorsSchema, vStringOrRegExpSchema } from '#valibot';
 
 const RULE_NAME = 'no-block-properties';
 
-const EXTERNAL_GEOMETRY_PROPERTIES = [
-	'inset',
-	'inset-block', 'inset-block-start', 'inset-block-end',
-	'inset-inline', 'inset-inline-start', 'inset-inline-end',
-	'top', 'right', 'bottom', 'left',
+/**
+ * TODO:
+ * * Per-entity allow/disallow
+ * * Custom presets?
+ * * Custom messages
+ */
 
-	'margin',
-	'margin-block', 'margin-block-start', 'margin-block-end',
-	'margin-inline', 'margin-inline-start', 'margin-inline-end',
-	'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
-];
+const PRESETS = {
+	EXTERNAL_GEOMETRY: new Set([
+		'margin',
+		'margin-block', 'margin-block-start', 'margin-block-end',
+		'margin-inline', 'margin-inline-start', 'margin-inline-end',
+		'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
+	]),
+	CONTEXT: new Set([
+		'float', 'clear',
+		'flex', 'flex-grow', 'flex-shrink', 'flex-basis',
+		'grid', 'grid-area',
+		'grid-row', 'grid-row-start', 'grid-row-end',
+		'grid-column', 'grid-column-start', 'grid-column-end',
+		'place-self', 'align-self',
+		'order',
+		'counter-increment',
+		'z-index',
+	]),
+	POSITIONING: new Set([
+		'position',
+		'inset',
+		'inset-block', 'inset-block-start', 'inset-block-end',
+		'inset-inline', 'inset-inline-start', 'inset-inline-end',
+		'top', 'right', 'bottom', 'left',
+	]),
+};
 
-const CONTEXTUAL_PROPERTIES = [
-	'float', 'clear',
+const propertyToPresetMap = new Map<string, keyof typeof PRESETS>();
 
-	'flex', 'flex-grow', 'flex-shrink', 'flex-basis',
-
-	'grid', 'grid-area',
-	'grid-row', 'grid-row-start', 'grid-row-end',
-	'grid-column', 'grid-column-start', 'grid-column-end',
-
-	'place-self',
-	'justify-content', 'justify-items', 'justify-self',
-	'align-self',
-	'order',
-
-	'counter-increment',
-	'z-index',
-];
-
-// const q = {
-// 	'no-top-level-properties': [true, {
-// 		disallow: ['{GEOMETRY}', '{CONTEXTUAL}'],
-// 	}],
-// };
+for (const [preset, properties] of tsObject.entries(PRESETS)) {
+	for (const property of properties) {
+		propertyToPresetMap.set(property, preset);
+	}
+}
 
 export default createRule({
 	name: addNamespace(RULE_NAME),
@@ -48,40 +56,94 @@ export default createRule({
 		fixable: false,
 	},
 	messages: {
-		unexpected: (propertyName: string) =>
-			`Unexpected external geometry property "${propertyName}" within a BEM block declaration.`,
+		unexpected: (
+			propertyName: string,
+			selector: string,
+			presetName: keyof (typeof PRESETS) | 'CUSTOM',
+		) => {
+			const propertyType = (() => {
+				if (presetName === 'EXTERNAL_GEOMETRY') return 'external geometry';
+				if (presetName === 'POSITIONING') return 'positioning';
+				if (presetName === 'CONTEXT') return 'contextual';
+				return '';
+			})();
+
+			return [
+				`Unexpected`,
+				propertyType,
+				`property "${propertyName}" within BEM Block selector ${selector}`,
+			].filter(Boolean).join(' ');
+		},
 	},
 	schema: {
 		primary: v.literal(true),
 		secondary: v.optional(
 			v.strictObject({
-				extraProperties: v.optional(v.array(v.string()), []),
+				presets: v.optional(
+					v.array(v.picklist(['EXTERNAL_GEOMETRY', 'CONTEXT', 'POSITIONING'])),
+					['EXTERNAL_GEOMETRY'],
+				),
+				allowProperties: v.optional(v.array(v.string()), []),
+				disallowProperties: v.optional(v.array(v.string()), []),
+				ignoreBlocks: v.optional(v.array(vStringOrRegExpSchema), []),
+				...vSeparatorsSchema,
 			}),
 		),
 	},
 }, (primary, secondary, { report, messages, root }) => {
+	// Add disallowed properties from `presets` (if any) and `disallowProperties`
+	const disallowedProperties = new Set([
+		...secondary.disallowProperties,
+		...secondary.presets.flatMap((presetName) => [...PRESETS[presetName]]),
+	]);
+	// Omit explicitly allowed ones
+	secondary.allowProperties.forEach((property) => disallowedProperties.delete(property));
+
+	const ignorePatterns = secondary.ignoreBlocks
+		.map((value) => toRegExp(value, { allowWildcard: true }));
+
 	root.walkRules((rule) => {
-		const resolvedSelector = resolveNestedSelector(rule.selector, rule)[0];
+		// Resolve any CSS or SASS nesting
+		// `&__element` -> `.the-component__element`
+		// `.the-component { .element {} }` -> `.the-component .element`
+		const resolvedSelector = resolveNestedSelector(rule.selector, rule as any)[0];
 
-		// Do not check not BEM-related rules.
-		if (!resolvedSelector.startsWith('.')) return;
-		// Do not check elements and all its contents.
-		if (resolvedSelector.includes('__')) return;
-		// Do not check side-effects like `.the-component .foo`, `.the-component span`
-		if (resolvedSelector.includes(' ')) return;
+		parseSelectors(resolvedSelector).forEach((selectorNodes) => {
+			// Side effect selector, `.foo span`, `span .foo`
+			if (selectorNodes.some((node) => node.type === 'combinator')) return;
+			// `.the-component::before` is allowed
+			if (isPseudoElementNode(selectorNodes.at(-1))) return;
 
-		const nodeChildDeclarations = findRuleDeclarations(rule, {
-			onlyDirectChildren: true,
-		});
+			const compoundSelectorPart = selectorNodes.toString();
+			const selectorBemEntities = resolveBemEntities(compoundSelectorPart, secondary);
 
-		nodeChildDeclarations.forEach((declaration) => {
-			if (EXTERNAL_GEOMETRY_PROPERTIES.includes(declaration.prop)) {
-				report({
-					message: messages.unexpected(declaration.prop),
-					node: declaration,
-					word: declaration.prop,
+			const entitiesToReport = selectorBemEntities
+				// We are looking only for BEM blocks or its modifiers in the rule
+				.filter((bemEntity) => !bemEntity.element)
+				// Skip ignored blocks
+				.filter((bemEntity) => {
+					return !ignorePatterns
+						.some((blockPattern) => blockPattern.test(bemEntity.block.value));
 				});
-			}
+
+			if (isEmpty(entitiesToReport)) return;
+
+			const declarationsToReport = findRuleDeclarations(rule, { onlyDirectChildren: true })
+				.filter((declaration) => disallowedProperties.has(declaration.prop));
+
+			entitiesToReport.forEach((bemEntity) => {
+				declarationsToReport.forEach((declaration) => {
+					report({
+						message: messages.unexpected(
+							declaration.prop,
+							bemEntity.selector.value,
+							propertyToPresetMap.get(declaration.prop) ?? 'CUSTOM',
+						),
+						node: declaration,
+						word: declaration.prop,
+					});
+				});
+			});
 		});
 	});
 });
