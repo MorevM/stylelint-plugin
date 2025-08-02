fbceb0fa4ebba79dc3e7aa7c209c1635638d3543
MorevM <max.seainside@gmail.com>
2025-08-03 02:54:01 +0300

    feat(modules/selectors): Expose `ResolvedNode` type

--- src/modules/selectors/types.ts
+++ src/modules/selectors/types.ts
@@ -1,1 +1,2 @@
 export type { ResolvedSelector } from './resolve-nested-selector/resolve-nested-selector.types';
+export type { ResolvedNode } from './resolve-selector-nodes/resolve-selector-nodes.types';
