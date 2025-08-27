7e53c2eb0dbb548015a7a169674a0771681ef24b
MorevM <max.seainside@gmail.com>
2025-08-28 01:01:36 +0300

    refactor(modules/postcss): Get rid of unused code

--- src/modules/postcss/get-rule-declarations/get-rule-declarations.ts
+++ src/modules/postcss/get-rule-declarations/get-rule-declarations.ts
@@ -1,13 +1,10 @@
-import { isNullish } from '@morev/utils';
 import type { Declaration, Rule } from 'postcss';
 
 type Options = {
-	filter: string | RegExp | null;
 	onlyDirectChildren: boolean;
 };
 
 const DEFAULTS = {
-	filter: null,
 	onlyDirectChildren: false,
 };
 
@@ -18,34 +15,17 @@ export const getRuleDeclarations = (
 	const declarations: Declaration[] = [];
 	const options = { ...DEFAULTS, ...userOptions };
 
-	if (isNullish(options.filter)) {
-		if (!options.onlyDirectChildren) {
-			// `.walkDecls()` traverses all the declarations.
-			rule.walkDecls((declaration) => { declarations.push(declaration); });
-			return declarations;
-		}
-
-		// `.each()` traverses only direct children.
-		rule.each((node) => {
-			if (node.type !== 'decl') return;
-			declarations.push(node);
-		});
-		return declarations;
-	}
-
 	if (!options.onlyDirectChildren) {
-		rule.walkDecls(options.filter, (declaration) => { declarations.push(declaration); });
+		// `.walkDecls()` traverses all the declarations.
+		rule.walkDecls((declaration) => { declarations.push(declaration); });
 		return declarations;
 	}
 
+	// `.each()` traverses only direct children.
 	rule.each((node) => {
 		if (node.type !== 'decl') return;
-
-		if (options.filter) {
-			node.prop.match(options.filter) && declarations.push(node);
-		} else {
-			declarations.push(node);
-		}
+		declarations.push(node);
 	});
+
 	return declarations;
 };
