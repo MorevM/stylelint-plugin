68dd2c3314aad505475b2607fd27c59d8d5ad5ce
MorevM <max.seainside@gmail.com>
2025-08-27 14:15:41 +0300

    feat(modules/shared): Add case constants that allows digit in front

--- src/modules/shared/constants/case-patterns/case-patterns.ts
+++ src/modules/shared/constants/case-patterns/case-patterns.ts
@@ -1,4 +1,11 @@
 export const KEBAB_CASE_REGEXP = /^[a-z][0-9a-z]*(?:-[0-9a-z]+)*$/;
+export const KEBAB_CASE_NUMERIC_REGEXP = /^[0-9a-z]+(?:-[0-9a-z]+)*$/;
+
 export const PASCAL_CASE_REGEXP = /^[A-Z][0-9a-z]*(?:[A-Z][0-9a-z]*)*$/;
+export const PASCAL_CASE_NUMERIC_REGEXP = /^[0-9A-Z][0-9a-z]*(?:[A-Z][0-9a-z]*)*$/;
+
 export const CAMEL_CASE_REGEXP = /^[a-z][0-9a-z]*(?:[A-Z][0-9a-z]*)*$/;
+export const CAMEL_CASE_NUMERIC_REGEXP = /^[0-9a-z]+(?:[A-Z][0-9a-z]*)*$/;
+
 export const SNAKE_CASE_REGEXP = /^[a-z][0-9a-z]*(?:_[0-9a-z]+)*$/;
+export const SNAKE_CASE_NUMERIC_REGEXP = /^[0-9a-z]+(?:_[0-9a-z]+)*$/;
