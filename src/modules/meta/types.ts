9e2c8755c861f0a5f931c5369636dc82ebe0cc61
MorevM <max.seainside@gmail.com>
2025-08-28 17:35:34 +0300

    feat(modules/meta): Add common Stylelint secondary options to each rule type

--- src/modules/meta/types.ts
+++ src/modules/meta/types.ts
@@ -2,90 +2,6 @@ import type { Severity } from 'stylelint';
 import type { Separators } from '#modules/shared';
 
 /**
- * Metadata describing a single Stylelint rule within the plugin.
- * Generated automatically from the actual rule sources.
- */
-export type RuleMeta = {
-	/**
-	 * Fully qualified rule ID.
-	 *
-	 * @example '@morev/bem/no-side-effects'
-	 */
-	id: string;
-
-	/**
-	 * Logical scope of the rule.
-	 * Used for grouping and documentation.
-	 *
-	 * @example 'base'
-	 * @example 'bem'
-	 * @example 'sass'
-	 */
-	scope: string;
-
-	/**
-	 * Short name of the rule without namespace or scope.
-	 *
-	 * @example 'no-side-effects'
-	 */
-	name: string;
-
-	/**
-	 * Human-readable description of the rule.
-	 *
-	 * @example 'Disallows selectors that apply styles outside the scope of the current BEM block.'
-	 */
-	description: string;
-
-	/**
-	 * Indicates whether the rule supports auto-fixes.
-	 *
-	 * @example true
-	 */
-	fixable: boolean;
-
-	/**
-	 * Relative path (from repo root) to the rule's documentation file.
-	 *
-	 * @example 'src/rules/bem/no-side-effects/no-side-effects.docs.md'
-	 */
-	docsPath: string;
-
-	/**
-	 * The path used by VitePress to link to the rule's documentation page.
-	 *
-	 * @example '/rules/bem/no-side-effects.md'
-	 */
-	vitepressPath: string;
-
-	/**
-	 * The link to the rule page used by VitePress.
-	 *
-	 * @example '/rules/bem/no-side-effects.html'
-	 */
-	vitepressLink: string;
-};
-
-/**
- * Type helper representing the configuration shape for a single rule.
- */
-export type RuleSetting<Primary, Secondary> =
-	| null
-	| Primary
-	| [null | Primary]
-	| [null | Primary, Secondary];
-
-/**
- * Global plugin settings that may apply to multiple rules.
- */
-export type PluginGlobals = {
-	/**
-	 * Defines the separators used to parse BEM class names.
-	 */
-	separators?: Separators;
-};
-
-/**
  * Types supported by every rule to ensure compatibility with Stylelint.
  *
  * @see https://stylelint.io/user-guide/configure/#rules
@@ -178,4 +94,88 @@ export type StylelintSecondaryOptions = {
 	 * ```
 	 */
 	severity?: Severity | ((...args: any[]) => Severity | null);
+};
+
+/**
+ * Metadata describing a single Stylelint rule within the plugin.
+ * Generated automatically from the actual rule sources.
+ */
+export type RuleMeta = {
+	/**
+	 * Fully qualified rule ID.
+	 *
+	 * @example '@morev/bem/no-side-effects'
+	 */
+	id: string;
+
+	/**
+	 * Logical scope of the rule.
+	 * Used for grouping and documentation.
+	 *
+	 * @example 'base'
+	 * @example 'bem'
+	 * @example 'sass'
+	 */
+	scope: string;
+
+	/**
+	 * Short name of the rule without namespace or scope.
+	 *
+	 * @example 'no-side-effects'
+	 */
+	name: string;
+
+	/**
+	 * Human-readable description of the rule.
+	 *
+	 * @example 'Disallows selectors that apply styles outside the scope of the current BEM block.'
+	 */
+	description: string;
+
+	/**
+	 * Indicates whether the rule supports auto-fixes.
+	 *
+	 * @example true
+	 */
+	fixable: boolean;
+
+	/**
+	 * Relative path (from repo root) to the rule's documentation file.
+	 *
+	 * @example 'src/rules/bem/no-side-effects/no-side-effects.docs.md'
+	 */
+	docsPath: string;
+
+	/**
+	 * The path used by VitePress to link to the rule's documentation page.
+	 *
+	 * @example '/rules/bem/no-side-effects.md'
+	 */
+	vitepressPath: string;
+
+	/**
+	 * The link to the rule page used by VitePress.
+	 *
+	 * @example '/rules/bem/no-side-effects.html'
+	 */
+	vitepressLink: string;
+};
+
+/**
+ * Type helper representing the configuration shape for a single rule.
+ */
+export type RuleSetting<Primary, Secondary> =
+	| null
+	| Primary
+	| [null | Primary]
+	| [null | Primary, Secondary & StylelintSecondaryOptions];
+
+/**
+ * Global plugin settings that may apply to multiple rules.
+ */
+export type PluginGlobals = {
+	/**
+	 * Defines the separators used to parse BEM class names.
+	 */
+	separators?: Separators;
 };
