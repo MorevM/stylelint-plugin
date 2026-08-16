983a27903b046dade45b5823e53dbe570d852f02
MorevM <max.seainside@gmail.com>
2026-08-16 09:37:45 +0300

    chore: Exercise all local Stylelint plugin rules

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from '@morev/stylelint-config';
+import localPlugin, { createDefineRules } from '@morev/stylelint-plugin';
 
-export default defineConfig({
+const tempFiles = ['temp/**/*.scss', 'temp/**/*.css'];
+const config = defineConfig({
 	preset: 'scss',
 	bem: {
-		files: ['temp/**/*.scss', 'temp/**/*.css'],
+		files: tempFiles,
 	},
 });
+
+const defineLocalRules = createDefineRules();
+const localRuleOverrides = defineLocalRules({
+	'@morev/bem/no-misplaced-side-effects': [true, {}],
+});
+
+const configuredRuleNames = new Set([
+	...Object.keys(config.rules ?? {}),
+	...(config.overrides ?? [])
+		.flatMap(({ rules }) => Object.keys(rules ?? {})),
+]);
+
+const localRuleNames = localPlugin.flatMap((plugin) => (
+	'ruleName' in plugin ? [plugin.ruleName] : []
+));
+
+const automaticallyEnabledRules = Object.fromEntries(
+	localRuleNames
+		.filter((ruleName) => !configuredRuleNames.has(ruleName))
+		.map((ruleName) => [ruleName, true]),
+);
+
+export default {
+	...config,
+	overrides: [
+		...(config.overrides ?? []),
+		{
+			files: tempFiles,
+			plugins: localPlugin,
+			rules: {
+				...automaticallyEnabledRules,
+				...localRuleOverrides,
+			},
+		},
+	],
+};
